3baa8f9efcaa65db15c53d78389af62dfbc2e89b
krzysztofprobulski <krzysztof.probulski@gmail.com>
2026-03-03 21:55:38 +0100

    Added Brute Force log simulation

--- src/main/java/com/krzpro/Main.java
+++ src/main/java/com/krzpro/Main.java
@@ -3,12 +3,18 @@ package com.krzpro;
 import com.krzpro.generator.IPGenerator;
 import com.krzpro.generator.LogGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            String log = LogGenerator.generateRandLog();
+        List<String> logs = new ArrayList<>();
+
+        logs = LogGenerator.generateLogs(100);
+
+        for (String log : logs) {
             System.out.println(log);
         }
     }
--- src/main/java/com/krzpro/generator/LogGenerator.java
+++ src/main/java/com/krzpro/generator/LogGenerator.java
@@ -1,12 +1,39 @@
 package com.krzpro.generator;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 public class LogGenerator {
-    public static String generateRandLog() {
+    static Random random = new Random();
+    public static List<String> generateLogs(int amount) {
+        List<String> logs = new ArrayList<>();
 
+        for (int count = 0; count < amount; ) {
+            int chance = random.nextInt(100);
+
+            if (chance < 5) {
+                int denied = random.nextInt(7) + 3;
+                String ip = IPGenerator.generateIP();
+                for (int i = 0; i < denied; i++) {
+                    Log log = new Log(ip, LocalDateTime.now(), Action.LOGIN, Status.FAILED);
+                    logs.add(log.toString());
+                    count++;
+                }
+                Log log = new Log(ip, LocalDateTime.now(), Action.LOGIN, Status.SUCCESS);
+                logs.add(log.toString());
+                count++;
+
+            } else {
+                logs.add(generateNormalLog());
+                count++;
+            }
+        }
+        return logs;
+    }
+    public static String generateNormalLog() {
         String ip = IPGenerator.generateIP();
         LocalDateTime time = LocalDateTime.now();
         Action action = Action.randomAction();
